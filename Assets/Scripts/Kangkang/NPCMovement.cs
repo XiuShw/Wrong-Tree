722b1f6a774b980570070ff1b1ca9cc390ab8631
XliuShw <hl20040806@qq.com>
2025-05-26 21:33:52 +0800

    NPC preparation

---
+++ Assets/Scripts/Kangkang/NPCMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class NPCMovement : MonoBehaviour
+{
+    Transform npcTransform;
+    [SerializeField] bool lightStealed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        npcTransform = GetComponentInParent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
