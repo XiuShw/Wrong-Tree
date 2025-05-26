dfe67683b34fd453723a9608af9663cea7e0e833
Ken <yongkang.cheng@mail.utoronto.ca>
2025-05-26 12:27:19 -0400

    add some NPC movement controller & animation

--- Assets/Scripts/Kangkang/NPCMovement.cs
+++ Assets/Scripts/Kangkang/NPCMovement.cs
@@ -2,17 +2,17 @@ using UnityEngine;
 
 public class NPCMovement : MonoBehaviour
 {
-    Transform npcTransform;
-    [SerializeField] bool lightStealed;
+	private Animator animator;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-    }
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		transform.Rotate(0, 1, 0);
-    }
+		
+	}
 }
